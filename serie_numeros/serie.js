5841d4cf9a51e349a449c9d6e967eb34634c525c
Fer-alejo2022 <fer.developer.1998@gmail.com>
2022-02-22 18:04:17 -0300

    updated

---
+++ serie_numeros/serie.js
@@ -0,0 +1,17 @@
+let numbers = [5];
+
+for (i = 0; i < 5; i++) {
+   numbers[i] = parseInt(prompt((i+1)+". Ingrese Número"));
+}
+
+// funcion que ordena los numeros crecientemente
+let creciente = numbers.sort(function(a,b){
+    return a-b;
+})
+// funcion que ordena los numeros decrecientemente
+let decreciente = numbers.sort(function(a,b){
+    return b-a;
+})
+
+// slice devuelve una parte del arreglo en otro arreglo
+document.write("Mayor : "+decreciente.slice(0,1)+" Menor : "+creciente.slice(4,5));
