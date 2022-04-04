6d9429784075ec2961f28518231121181e09fa09
Fer-alejo2022 <fer.developer.1998@gmail.com>
2022-04-04 15:34:02 -0400

    updated

---
+++ alert_rain/rain.js
@@ -0,0 +1,7 @@
+function avisoLluvia(estaLloviendo) {
+      let estaLloviendo = true;
+      let bolean = estaLloviendo == true ? "Esta Lloviendo" : "No esta Lloviendo";
+      return bolean;             
+}
+let yes_no;
+alert(avisoLluvia(yes_no));
